51ab942564a87995dddfcaab0e23acfef0b6692c
Nigorjeanluc <nigorjeanluc@gmail.com>
2019-09-26 12:18:57 +0200

    ft(user-signin): build api for signin - ensure that the res status are appropriate - ensure that the user already has an account - ensure that the user has a correct password [Finishes #168778891]

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,5 @@ import userController from '../controllers/userController';
 const router = express.Router();
 
 router.post('/api/v1/signup', userController.signUp);
-
+router.post('/api/v1/auth/signin', userController.signIn);
 export default router;
