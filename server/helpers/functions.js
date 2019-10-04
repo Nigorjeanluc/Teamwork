cd82d131739a16acd2faa881a7492eeaef914295
Nigorjeanluc <nigorjeanluc@gmail.com>
2019-10-04 14:54:32 +0200

    ft(test-email-random-string): add randomString to helper functions [Finishes #168948763]

--- server/helpers/functions.js
+++ server/helpers/functions.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const func = {
     randomString: (length) => {
-        const result = '';
+        let result = '';
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         const charactersLength = characters.length;
         for (let i = 0; i < length; i++) {
